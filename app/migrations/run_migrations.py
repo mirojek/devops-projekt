4b906905907a1b0ee76462f86fe31e304a894eb2
mirojek <mirojek@student.wszib.edu.pl>
2025-12-19 04:19:28 +0100

    Migration runner service w dockerze

---
+++ app/migrations/run_migrations.py
@@ -0,0 +1,11 @@
+from app.src import create_app, db
+from app.src.app import User
+
+def run():
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+        print("Migrations: tables created")
+
+if __name__ == "__main__":
+    run()
